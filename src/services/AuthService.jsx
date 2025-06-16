d0fd6ec78673e84dc5e956dcafa6331557ecb8fa
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-06-16 20:07:21 +0530

    assigne Elastic IP

--- src/services/AuthService.jsx
+++ src/services/AuthService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/api/auth";
+const API = "http://34.200.96.211:8080/api/auth";
 
 export const signup = (user) => axios.post(`${API}/signup`, user);
 export const login = (user) => axios.post(`${API}/login`, user);
--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080/api/employees";
+const API = "http://34.200.96.211:8080/api/employees";
 const token = () => localStorage.getItem("token");
 
 export const addEmployee = (employee) =>
